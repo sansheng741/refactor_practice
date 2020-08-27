ed2d8fbfc1bedbdb94328a08baec97f8e463dc86
sansheng741 <807156237@qq.com>
2020-08-27 17:12:46 +0800

    [refactor_practice] [colin] test : create test htmlStatement case 8 print html

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,4 +1,5 @@
 const test = require('ava');
+const {htmlStatement} = require("../src/statement");
 const {statement} = require('../src/statement');
 
 const plays = {
@@ -161,3 +162,37 @@ test('statement case 7. Customer BigCo has one unknown performance. ', t => {
         t.is(e.message, 'unknown type: tragedy1');
     }
 });
+
+
+test('htmlStatement case 8 print html', t =>{
+    const invoice = {
+        'customer': 'BigCo',
+        'performances': [
+            {
+                'playID': 'hamlet',
+                'audience': 55,
+            },
+            {
+                'playID': 'as-like',
+                'audience': 35,
+            },
+            {
+                'playID': 'othello',
+                'audience': 40,
+            },
+        ],
+    };
+
+    const result = htmlStatement(invoice, plays);
+
+    t.is(result, '<h1>Statement for BigCo</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n');
+
+})
